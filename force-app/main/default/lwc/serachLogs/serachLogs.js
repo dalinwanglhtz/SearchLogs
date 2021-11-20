d297290101d54b189c5962d69788315635008078
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-11-20 21:03:32 +1100

    Add spinning wheel

--- force-app/main/default/lwc/serachLogs/serachLogs.js
+++ force-app/main/default/lwc/serachLogs/serachLogs.js
@@ -3,17 +3,24 @@ import searchLogs from '@salesforce/apex/SearchLogs.searchLogs';
 
 export default class SerachLogs extends LightningElement {
     someData;
+    isLoaded = true;
 
     handleSearch() {
+        this.isLoaded = false;
         let searchText = this.template.querySelector('lightning-input').value;
-        if(!searchText) return;
+        if(!searchText) {
+            this.isLoaded = true;
+            return;
+        }
         searchLogs({searchStr : searchText})
             .then((result) => {
                 console.log('log data: ', result);
+                this.isLoaded = true;
                 this.someData = result;
             })
             .catch((error) => {
+                this.isLoaded = true;
                 console.log('Error with search: ', error);
-            })
+            });
     }
 }
