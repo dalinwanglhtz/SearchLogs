e259addec97be5e9b70f525dcf682ea529f2ead5
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-11-21 20:30:47 +1100

    Create new UI for Api User and add method for register Api user

---
+++ force-app/main/default/lwc/apiUserRegister/apiUserRegister.js
@@ -0,0 +1,34 @@
+import { LightningElement } from 'lwc';
+import apiUserRegister from '@salesforce/apex/SearchLogs.apiUserRegister';
+
+export default class ApiUserRegister extends LightningElement {
+
+    handleRegister() {
+        let apiUser = {}
+        let inputs = this.template.querySelectorAll('lightning-input');
+        inputs.forEach(element => {
+            if(element.name == 'username') {
+                apiUser.Username__c = element.value;
+            } else if(element.name == 'password') {
+                apiUser.Password__c = element.value;
+            } else if(element.name == 'clientId') {
+                apiUser.Client_ID__c = element.value;
+            } else if(element.name == 'clientSecret') {
+                apiUser.Client_Secret__c = element.value;
+            } else if(element.name == 'currentOrg') {
+                apiUser.Current_Org_Site__c = element.value;
+            }
+        });
+        this.registerApiUser(apiUser);
+    }
+
+    registerApiUser(apiUser) {
+        apiUserRegister({apiUser: apiUser})
+            .then((result) => {
+                console.log('Success');
+            })
+            .catch((error) => {
+                console.log('Error: ', error);
+            })
+    }
+}
